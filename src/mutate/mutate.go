0c7bb67c803413597b497ed0b6839d9d559f4978
BrokenKeyboard <satasiyahet@gmail.com>
2021-02-24 19:34:15 +0000

    You can now modify variables using the mutate package

--- src/mutate/mutate.go
+++ src/mutate/mutate.go
@@ -1,3 +1,27 @@
 package mutate
+
+import (
+	"reflect"
+)
 import("../common")
-var Funcs = []common.Identifier{}
+var Funcs = []common.Identifier{common.Identifier{Function:vmod, Name:"vmod"}}
+func add(int1 int, int2 int) int{
+	return int1 + int2
+}
+func getUnderlyingAsValue(data interface{}) reflect.Value {
+	return reflect.ValueOf(data)
+}
+
+func vmod(vars map[string]*common.Variable, line string){
+	parsedline := common.Parse(line)
+	thing := vars[parsedline[1]].Value
+	switch c := thing.(type){
+	case int:
+		out := c + common.Strtoint(parsedline[2])
+		vars[parsedline[1]].Value = out
+	case string:
+		out := c + (parsedline[2])
+		vars[parsedline[1]].Value = out
+	}
+	// vars[parsedline[1]].Value = vars[parsedline[1]].Value + common.Strtoint(parsedline[2])
+}
